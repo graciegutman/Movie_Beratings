7d4f416ffa9276330d157e5a4c20f7b3d85aa557
Hackbright Student <info@hackbrightacademy.com>
2014-03-06 17:43:37 -0800

    querying practice

--- model.py
+++ model.py
@@ -29,6 +29,18 @@ class Movie(Base):
     released_at = Column(DateTime, nullable=True)
     imdb_url = Column(String(64), nullable=True)
 
+    # al = session.query(model.Movie).filter_by(title = "Aladdin").first()
+    # al = s.query(Movie).filter_by("id<5").all() <-- syntax shit
+    # al = s.query(Movie).filter("id<5").all()
+
+al = s.query(Movie).filter(Movie.name.in_(s.query(Movie.name).filter(Movie.name.like('B%')))).all()
+al = s.query(Movie).filter(Movie.released_at.like("1992%")).all()
+
+
+
+
+al = s.query(Movie).filter(Movie.released_at.in_(s.query(Movie.released_at).filter("1970 < Movie.released_at[0] < 1973")).all()
+
 class Rating(Base):
 
     __tablename__ = "ratings"
--- seed.py
+++ seed.py
@@ -29,10 +29,19 @@ def load_movies(session):
         # parse and clean up the line
         line = raw_line.split('|')
         clean_line = [x.strip().decode("latin-1") for x in line]
+
+        # freaking release dates are with the titles
+        # This is dumb. If only we knew how to use Regex. 
+        original_name_and_release_date = clean_line[1]
+        for i in range(len(original_name_and_release_date)):
+            # MAGIC for Scream of Stone (Schrei aus Stein) (1991). What?
+            if original_name_and_release_date[i] in ["("]:
+                name = original_name_and_release_date[:i-1]
+
         # create object
         if clean_line[2]:
             datetime_format = datetime.datetime.strptime(clean_line[2], "%d-%b-%Y")
-        new_movie = model.Movie(name=clean_line[1], released_at=datetime_format, imdb_url=clean_line[4])
+        new_movie = model.Movie(name=name, released_at=datetime_format, imdb_url=clean_line[4])
         # add object to session
         session.add(new_movie)
     # commit
@@ -57,9 +66,9 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    load_users(s)
+    # load_users(s)
     load_movies(s)
-    load_ratings(s)
+    # load_ratings(s)
 
 if __name__ == "__main__":
     s = model.connect()
